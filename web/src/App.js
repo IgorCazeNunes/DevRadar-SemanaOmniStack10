b4ade1e245b69efda739f3c75cbacbf7e2acc63c
Igor Cazé Nunes <igorcaze@gmail.com>
2020-01-15 15:13:31 -0300

    Adicionado Conexão com a API.

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import'./global.css';
 import'./App.css';
@@ -6,6 +7,8 @@ import'./Sidebar.css';
 import'./Main.css';
 
 function App() {
+	const [github_username, setGithubUsername] = useState('');
+	const [techs, setTechs] = useState('');
 	const [latitude, setLatitude] = useState('');
 	const [longitude, setLongitude] = useState('');
 
@@ -34,19 +37,30 @@ function App() {
 				<form>
 					<div className="input-block">
 						<label htmlFor="github_username">Usuário do Github</label>
-						<input name="github_username" id="github_username" required />
+						<input 
+							name="github_username" 
+							id="github_username" 
+							value={github_username}
+							onChange={e => setGithubUsername(e.target.value)}
+							required 
+						/>
 					</div>
 
 					<div className="input-block">
 						<label htmlFor="techs">Tecnologias</label>
-						<input name="techs" id="techs" required />
+						<input 
+							name="techs" 
+							id="techs" 
+							value={techs}
+							onChange={e => setTechs(e.target.value)}
+							required 
+						/>
 					</div>
 
 					<div className="input-group">
 						<div className="input-block">
 							<label htmlFor="latitude">Latitude</label>
 							<input 
-								type="number"
 								name="latitude" 
 								id="latitude" 
 								value={latitude} 
@@ -58,7 +72,6 @@ function App() {
 						<div className="input-block">
 							<label htmlFor="longitude">Longitude</label>
 							<input 
-								type="number" 
 								name="longitude" 
 								id="longitude"
 								value={longitude} 
