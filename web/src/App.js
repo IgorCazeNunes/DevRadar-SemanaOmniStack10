43bd65bc5eb3bb55987b8b03276d50c63760712d
Igor Cazé Nunes <igorcaze@gmail.com>
2020-01-15 13:22:52 -0300

    Adicionado arquivos para desenvolvimento do layout basico

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,16 @@
 import React, { useState } from 'react';
 
-import Header from './Header';
-
-/*
-	Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-	Propriedade: Informações que um componente PAI passa para o componente FILHO
-	Estado: Informações mantidas pelo componente (Imutabilidade)
-*/
-
 function App() {
-	const [counter, setCounter] = useState(0);
-
-	function incrementCounter() {
-		setCounter(counter + 1);
-	}
-
 	return (
-		<>
-			<Header title="Dashboard" />
-			<h1>Contador: {counter}</h1>
-			<button onClick={incrementCounter}>Incrementar</button>
-		</>
+		<div id="app">
+			<aside>
+
+			</aside>
+
+			<main>
+				
+			</main>
+		</div>
 	);
 }
 
