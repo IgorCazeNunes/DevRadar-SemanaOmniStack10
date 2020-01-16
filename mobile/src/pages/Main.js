6df83ecaa18a2d11c378b1481b0d7baa78938710
Igor Cazé Nunes <igorcaze@gmail.com>
2020-01-16 15:54:56 -0300

    Adicionado formulario de pesquisa ao Main.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,21 +35,37 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}> 
-            <Marker coordinate={{ latitude: -9.391282, longitude: -40.4698979 }} >
-                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/30866274?s=460&v=4https://avatars0.githubusercontent.com/u/30866274?s=460&v=4' }} />
-                
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'IgorCazeNunes' })
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Igor Cazé</Text>
-                        <Text style={styles.devBio}>UAU</Text>
-                        <Text style={styles.devTechs}>Spring, Spring.Boot, Angular 6</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}> 
+                <Marker coordinate={{ latitude: -9.391282, longitude: -40.4698979 }} >
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/30866274?s=460&v=4https://avatars0.githubusercontent.com/u/30866274?s=460&v=4' }} />
+                    
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'IgorCazeNunes' })
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Igor Cazé</Text>
+                            <Text style={styles.devBio}>UAU</Text>
+                            <Text style={styles.devTechs}>Spring, Spring.Boot, Angular 6</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFFFFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     );
 }
 
@@ -82,7 +99,43 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5,
-    }
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFFFFF',
+        color: '#333333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+    },
 })
 
 export default Main;
